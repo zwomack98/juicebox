014a3ba45e960b78a1fd0ed00d51ad4ba01f5af9
zwomack98 <83146317+zwomack98@users.noreply.github.com>
2021-06-10 12:30:50 -0500

    commit

---
+++ db/index.js
@@ -0,0 +1,30 @@
+const { Client } = require('pg') // imports the pg module
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  const { rows } = await client.query(`SELECT id, username FROM users;`);
+
+  return rows;
+}
+
+module.exports = {  
+  client,
+  createUser,
+  getAllUsers,
+}
